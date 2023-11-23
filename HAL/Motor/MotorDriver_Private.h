5bd25c46394d8665022e9d820be26d6677882490
Ahmed Salah <a.salahhelmy2001@gmail.com>
2023-11-23 16:31:44 +0200

    Motor driver to control Motors directions

---
+++ HAL/Motor/MotorDriver_Private.h
@@ -0,0 +1,12 @@
+/**
+ * @file MotorDriver_Private.h
+ * @brief Private file for the motor driver.
+ *
+ * This file is the private header file for the motor driver module.
+ * It contains private declarations and definitions specific to the motor driver implementation.
+ */
+
+#ifndef HAL_MOTORDRIVER_MOTORDRIVER_PRIVATE_H_
+#define HAL_MOTORDRIVER_MOTORDRIVER_PRIVATE_H_
+
+#endif /* HAL_MOTORDRIVER_MOTORDRIVER_PRIVATE_H_ */

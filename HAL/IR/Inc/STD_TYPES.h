b2ea288ef9795e60022fb77b8cf4de9e8f4e5f1d
Ahmed Salah <a.salahhelmy2001@gmail.com>
2023-12-06 17:18:15 +0200

    Adding IR driver

---
+++ HAL/IR/Inc/STD_TYPES.h
@@ -0,0 +1,25 @@
+/*****************************************************************************/
+/*
+*Author: Reem 
+*Date: 3/11/2021
+*Version: 1
+*/
+/****************************************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+#define NULL ( (void *) 0)
+
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed long int s32;
+
+typedef float f32;
+typedef double f64;
+
+#endif
